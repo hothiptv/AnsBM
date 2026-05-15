8a8c68af3119f709efa78b2f71b06a8b399d6738
hothiptv <hothiptvnek@gmail.com>
2026-05-15 13:06:45 +0700

    Offline King: Sync data - 2026-05-15T06:06:44.838Z

--- data.js
+++ data.js
@@ -10,17 +10,6 @@
       "tickets": 999,
       "luck": 100,
       "lastSaved": "2026-05-15T06:03:05.489Z"
-    },
-    {
-      "username": "khánh",
-      "passwordHash": "khanh",
-      "pin": "12345",
-      "level": 999,
-      "exp": 0,
-      "coins": 0,
-      "tickets": 5,
-      "luck": 10,
-      "lastSaved": "2026-05-15T06:02:39.372Z"
     }
   ]
 }
