d7707d66c602a5ab7251a96e3014e895d39f1652
hothiptv <hothiptvnek@gmail.com>
2026-05-15 13:01:59 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,22 @@
+{
+  "accounts": [
+    {
+      "username": "an",
+      "password": "ad090",
+      "pin": "00000",
+      "stats": {
+        "level": 99,
+        "exp": 0,
+        "next_level_exp": 1000,
+        "coins": 999999,
+        "tickets": 999,
+        "luck": 100
+      }
+    }
+  ],
+  "config": {
+    "version": "1.0",
+    "last_update": "2026-05-15"
+  }
+}
 
